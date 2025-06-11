39a42de7d4af080729f7603c167d2162e38777f0
musa <abdulmusawwirshaik@gmail.com>
2025-06-11 11:56:29 +0530

    updated

--- src/main/java/com/example/CrudProject/model/Student.java
+++ src/main/java/com/example/CrudProject/model/Student.java
@@ -2,11 +2,14 @@ package com.example.CrudProject.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Entity
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
